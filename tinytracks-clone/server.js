177d584ac420cd9d478a56be65099a0cd996f263
Phil <121241622+philhub27@users.noreply.github.com>
2025-04-01 12:11:03 +0200

    Add tinytracks-clone/server.js 1743502264761

---
+++ tinytracks-clone/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Handle all routes by serving index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
